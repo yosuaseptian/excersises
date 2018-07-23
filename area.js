dbd57d918adde40d2638ad50af1310f5683bd5d8
yosuaspetian <ys_jojooo@yahoo.com>
2018-07-23 21:46:42 +0700

    (test)

---
+++ area.js
@@ -0,0 +1,6 @@
+var pi = require('./areaOfCircle')
+var square = require('./areaOfSquare')
+
+var area = pi() * square()
+
+console.log(area);
---
+++ areaOfSquare.js
@@ -0,0 +1,7 @@
+function square() {
+    return 7
+}
+
+console.log(square(7))
+
+module.exports = square
